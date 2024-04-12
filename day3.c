8d7c88070791f2ba983d19eb6e827afaeaaa47f8
bhanupratap30 <manzansh3008@gmail.com>
2024-04-12 23:29:48 +0530

    Third Commit

---
+++ day3.c
@@ -0,0 +1,39 @@
+/*Mr. Liam is an aspiring programmer exploring the use of logical operators. Create a simple
+program to assist Liam in understanding logical operations.Prompt two integers, a and b, and 
+use logical operators to determine whether both values are non-zero. Display 'True' if both 
+values are non-zero; otherwise, display 'False'.*/
+
+
+// #include <stdio.h>
+// int main(){
+//     int a, b;
+//     printf("Enter the value of a:\n");
+//     scanf("%d", &a);
+//     printf("Enter the value of b:");
+//     scanf("%d", &b);
+//     if (a != 0 && b != 0) {
+//         printf("True");
+//     } else {
+//         printf("False");
+//     }
+// return 0;
+// }
+
+/*alexa is developing a number validation program using logical operators and seeks your assistance.
+Develop a program that checks if a given integer meets specific criteria. If the number is positive 
+or even, the program should output "Valid"; otherwise, it should output "Invalid"*/
+
+// #include <stdio.h>
+// int main() {
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d", &a);
+//     if (a > 0 || a % 2 == 0) {
+//         printf("Its valid");
+//     } else{
+//         printf("Its invalid");
+//     }
+//     return 0;
+// }
+
+/**/
