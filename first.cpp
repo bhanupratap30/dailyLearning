1bb7ede62ba4d3989242f1bacead77e9cda56003
Bhanu Pratap Singh <150129308+bhanupratap30@users.noreply.github.com>
2024-06-05 22:04:00 +0530

    Intial commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"hey bhanu\n";
+//     cout<<"Learning C language is far easier than C++"<<endl<<"okkk vaiii";
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+  cout << "Hello World!" << "\n\n";
+  cout << "I am learning C++";
+  return 0;
+}
